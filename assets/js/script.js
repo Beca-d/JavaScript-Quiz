c1a01d6e375d03a458b246609468d8c42a2cf8cf
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-06-05 16:50:54 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+var quizQuestions = [
+    {
+        question: "What is the output of this code? console.log(typeof typeof 1);",
+        answers: {
+            a: 'string',
+            b: 'true',
+            c: 'number',
+            d: '1'
+        },
+        correctanswer: 'a'
+    },
+    {
+        question: "Arrays in java are:",
+        answers: {
+            a: 'Null values',
+            b: 'numbers',
+            c: 'object references',
+            d: 'objects'
+        },
+        correctanswer: 'd'
+    },
+    {
+        question: "What is the output of this code? console.log(1 + "2" + "2");",
+        answers: {
+            a: '32',
+            b: '5',
+            c: '122',
+            d: 'NaN'
+        },
+        correctanswer: 'c'
+    },
+    {
+        question: "JavaScript file has an extension of:",
+        answers: {
+            a: '.js',
+            b: '.java',
+            c: '.javacript',
+            d: '.json'
+        },
+        correctanswer: 'a'
+    },
+    {
+        question: "How do you properly callback a function?",
+        answers: {
+            a: 'callback function()',
+            b: 'function()',
+            c: 'function(call)',
+            d: 'call [function ()]'
+        },
+        correctanswer: 'a'
+    }
+
+];
+
